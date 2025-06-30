f83defbcd82ebec274d2f641989eff86a05ffb97
Devendar Singh Rawat <devendasinghrawat925@gmail.com>
2025-06-30 13:12:10 +0530

    restored webcontainer.js

--- frontend/src/config/webContainer.js
+++ frontend/src/config/webContainer.js
@@ -1,16 +1,11 @@
-let cleanupTimeout;
+import { WebContainer } from '@webcontainer/api';
+
+let webContainerInstance = null;
+
 
 export const getWebContainer = async () => {
     if (webContainerInstance === null) {
         webContainerInstance = await WebContainer.boot();
     }
-
-    // Reset cleanup timeout
-    if (cleanupTimeout) clearTimeout(cleanupTimeout);
-    cleanupTimeout = setTimeout(() => {
-        webContainerInstance = null; // Release instance
-        console.log("WebContainer released due to inactivity.");
-    }, 60000); // 60 seconds of inactivity
-
     return webContainerInstance;
-};
+}
